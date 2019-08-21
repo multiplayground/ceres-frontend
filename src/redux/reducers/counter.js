0c4951592e4ee4b210f66d785f90a5316e7cb24a
TarasovOlegV <pgsmall1@gmail.com>
2019-08-21 22:01:02 +0400

    fix: del unnecessary action creators

--- src/redux/reducers/counter.js
+++ src/redux/reducers/counter.js
@@ -1,18 +1,6 @@
 const INC_COUNT = 'INC_COUNT';
 const DEC_COUNT = 'DEC_COUNT';
 
-export function incCount() {
-  return {
-    type: INC_COUNT
-  };
-}
-
-export function deleteListItem() {
-  return {
-    type: DEC_COUNT
-  };
-}
-
 export default function reducer(state = 0, action) {
   switch (action.type) {
     case INC_COUNT:
